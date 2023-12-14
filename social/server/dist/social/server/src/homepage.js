c8d9f37767a3c68164dcd243bcfe237847eff9ec
Graham <graham.held@gmail.com>
2023-12-14 03:33:00 +0000

    remove lingering dist files

--- social/server/dist/social/server/src/homepage.js
+++
@@ -1,5 +0,0 @@
-import express from 'express';
-import path from 'path';
-const router = express.Router();
-router.use(express.static(path.resolve(process.cwd(), '../../web/dist')));
-export default router;
